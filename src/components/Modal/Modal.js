b8a6e56b8cb288120f4246b22d42fca293ee2ec4
Cactus <ysjungman@gmail.com>
2023-06-10 14:44:44 +0900

    386_전환컴포넌트_감싸기

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
 const modal = (props) => {
-    const cssClasses = [
-        'Modal',
-        props.show === 'entering'
-            ? 'ModalOpen'
-            : props.show === 'exiting'
-                ? 'ModalClosed'
-                : null
-    ];
-    return (
-        <div className={cssClasses.join(' ')}>
+  return (
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={300}
+    >
+      {state => {
+        const cssClasses = [
+          'Modal',
+          state === 'entering'
+            ? "ModalOpen" : state === "exiting"
+              ? "ModalClosed" : null
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
             <h1>A Modal</h1>
             <button className="Button" onClick={props.closed}>
-                Dismiss
+              Dismiss
             </button>
-        </div>
-    );
+          </div>
+        );
+      }}
+    </Transition>
+  );
 };
 
 export default modal;
